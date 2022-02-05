63c515382e4f7114766f3ec7e760bcc7d5c7928c
Dedicated407 <ilyaboy@list.ru>
2022-02-05 17:44:34 +0300

    Add indexes for products, categories

--- SunriseClothingStore/Models/StoreContext.cs
+++ SunriseClothingStore/Models/StoreContext.cs
@@ -4,11 +4,22 @@ namespace SunriseClothingStore.Models;
 
 public class StoreContext : DbContext
 {
-    public StoreContext(DbContextOptions<StoreContext> options) : base(options) { }
-    
+    public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+    {
+    }
+
     public DbSet<Product> Products { get; set; }
     public DbSet<Category> Categories { get; set; }
     public DbSet<Order> Orders { get; set; }
     public DbSet<OrderLine> OrderLines { get; set; }
-    
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>().HasIndex(product => product.Name);
+        modelBuilder.Entity<Product>().HasIndex(product => product.PurchasePrice);
+        modelBuilder.Entity<Product>().HasIndex(product => product.SalePrice);
+        modelBuilder.Entity<Category>().HasIndex(category => category.Name);
+        modelBuilder.Entity<Category>().HasIndex(category => category.Description);
+    }
+
 }
